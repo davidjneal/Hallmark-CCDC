748294c83901244e9231a8f4c258f539af98045b
David Neal <david@cyberhero.io>
2016-05-16 13:48:50 -0400

    started baseline & lock scripts

---
+++ scripts/baseline/linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+netstat -plnt
+
+iptables -nL
+
+cat /etc/sudoers
+
+cat /etc/hosts.allow
+cat /etc/hosts.deny
+
+cat /etc/ssh/sshd_config
---
+++ scripts/lock/imute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+chmod +i /etc/sudoers
+
+chmod +i /etc/security/*
+
+chmod +i /etc/passwd
+
+chmod +i /etc/shadow
+
+chmod +i /etc/hosts
+
+chmod +i /etc/hosts.allow
+
+chmod +i /etc/hosts.deny
+
+chmod +i /var/spool/cron/*
