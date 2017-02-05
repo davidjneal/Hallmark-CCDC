d673d41bdbb85b531fa3ab1dace1d4432fc1d666
David Neal <david@cyberhero.io>
2017-02-04 21:09:09 -0600

    updated stuff

--- scripts/lock/unimute.sh
+++ scripts/lock/unimute.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-chmod -i /etc/sudoers
+chattr -i /etc/sudoers
 
-chmod -i /etc/security/*
+chattr -i /etc/security/*
 
-chmod -i /etc/passwd
+chattr -i /etc/passwd
 
-chmod -i /etc/shadow
+chattr -i /etc/shadow
 
-chmod -i /etc/hosts
+chattr -i /etc/hosts
 
-chmod -i /etc/hosts.allow
+chattr -i /etc/hosts.allow
 
-chmod -i /etc/hosts.deny
+chattr -i /etc/hosts.deny
 
-chmod -i /var/spool/cron/*
+chattr -i /var/spool/cron/*
